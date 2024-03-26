98ce4133e02a6166544070013278f13a7bb19589
xizana <xizoooo102@gmail.com>
2024-03-26 12:17:03 +0400

    Initial Commit

---
+++ MapKit & Core Location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKit & Core Location
+//
+//  Created by Lasha Khizanishvili on 26.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
